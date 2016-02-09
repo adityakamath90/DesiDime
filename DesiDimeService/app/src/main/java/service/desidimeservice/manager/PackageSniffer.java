504274a5eb5342f5f0bbe20c9087c2410f22f141
Aditya Kamath <diego.developers@robosftin.com>
2016-02-09 16:27:27 +0530

    -  Logic working successfully without permission in Android M

--- DesiDimeService/app/src/main/java/service/desidimeservice/manager/PackageSniffer.java
+++ DesiDimeService/app/src/main/java/service/desidimeservice/manager/PackageSniffer.java
@@ -1,13 +1,17 @@
 package service.desidimeservice.manager;
 
 import android.app.ActivityManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 import java.util.List;
+import java.util.SortedMap;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.TreeMap;
 
 import service.desidimeservice.service.IconService;
 import service.desidimeservice.utility.Constants;
@@ -23,8 +27,6 @@ public class PackageSniffer {
     public void startSniffingForPakage() {
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
-            int phonelaunched = 0, phoneclosed = 0;
-            int phonelaunches = 1;
 
             @Override
             public void run() {
@@ -38,26 +40,37 @@ public class PackageSniffer {
 
                     for (int packageCounter = 0; packageCounter < Constants.PACKAGES.length;
                          packageCounter++) {
-                        if (appProcess.processName.equals(Constants.PACKAGES[packageCounter])) {
-                            if (appProcess.importance == ActivityManager.RunningAppProcessInfo
-                                    .IMPORTANCE_FOREGROUND) {
-                                if (phonelaunched == 0) {
-                                    phonelaunched = 1;
-                                    startIconService(appProcess.processName);
-                                } else if (phoneclosed == 1) {
-                                    phonelaunches++;
-                                    phoneclosed = 0;
+                        String currentApp = "NULL";
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES
+                                .LOLLIPOP) {
+                            UsageStatsManager usm = (UsageStatsManager) mContext.getSystemService
+                                    (Context.USAGE_STATS_SERVICE);
+                            long time = System.currentTimeMillis();
+                            List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager
+                                    .INTERVAL_DAILY, time - 1000 * 1000, time);
+                            if (appList != null && appList.size() > 0) {
+                                SortedMap<Long, UsageStats> mySortedMap = new TreeMap<>();
+                                for (UsageStats usageStats : appList) {
+                                    mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
                                 }
-                            } else {
-                                phoneclosed = 1;
+                                if (mySortedMap != null && !mySortedMap.isEmpty()) {
+                                    currentApp = mySortedMap.get(mySortedMap.lastKey())
+                                            .getPackageName();
+                                }
                             }
+                        } else {
+                            List<ActivityManager.RunningAppProcessInfo> tasks = am
+                                    .getRunningAppProcesses();
+                            currentApp = tasks.get(0).processName;
                         }
+                        startIconService(currentApp);
                     }
 
 
                 }
             }
         }, 2000, 3000);
+
     }
 
     private void startIconService(String processName) {
@@ -67,23 +80,31 @@ public class PackageSniffer {
         switch (processName) {
             case Constants.AMAZON: {
                 currentPackage = Constants.AMAZON;
+                Intent intent = new Intent(mContext, IconService.class);
+                intent.putExtra(Constants.PACKAGE_NAME, currentPackage);
+                mContext.startService(intent);
             }
             break;
 
             case Constants.FLIPKART: {
                 currentPackage = Constants.FLIPKART;
+                Intent intent = new Intent(mContext, IconService.class);
+                intent.putExtra(Constants.PACKAGE_NAME, currentPackage);
+                mContext.startService(intent);
             }
             break;
 
             case Constants.LATEX_MOBILE: {
                 currentPackage = Constants.LATEX_MOBILE;
+                Intent intent = new Intent(mContext, IconService.class);
+                intent.putExtra(Constants.PACKAGE_NAME, currentPackage);
+                mContext.startService(intent);
             }
             break;
 
         }
-        Intent intent = new Intent(mContext, IconService.class);
-        intent.putExtra(Constants.PACKAGE_NAME, currentPackage);
-        mContext.startService(intent);
+
     }
+
 
 }
